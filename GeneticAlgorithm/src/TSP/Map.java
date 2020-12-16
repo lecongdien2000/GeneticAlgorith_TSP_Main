7606bcff6bfe61097c8486de0d9a0e8ed5d1eb20
hanthuy200300 <75974502+hanthuy200300@users.noreply.github.com>
2020-12-16 22:02:02 +0700

    update Map.java

--- GeneticAlgorithm/src/TSP/Map.java
+++ GeneticAlgorithm/src/TSP/Map.java
@@ -3,8 +3,11 @@ package TSP;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Map {
 	private double[][] map;
@@ -35,18 +38,43 @@ public class Map {
 		do {
 			str = bf.readLine();
 		} while (!str.equals("NODE_COORD_SECTION"));
-		// List<Point> list
-		// while(str!=EOF)
-		// str = readLine()
-		// strArr = split(str, " ");
-		// list.addX(toDouble(strArr[1]))
-		// list.addY(toDouble(strArr[2]));
-
+		List<Point> list = new ArrayList<Point>();
+		while (true) {
+			str = bf.readLine();
+			if (str.equals("EOF"))
+				break;
+			String[] strArr = str.trim().split("\\s+");
+			System.out.println(strArr[1]);
+			list.add(new Point(Double.parseDouble(strArr[1]), Double.parseDouble(strArr[2])));
+		}
+		map = new double[list.size()][list.size()];
+		for (int i = 0; i < list.size() - 1; i++) {
+			// list.size - i - 1
+			for (int j = i + 1; j < list.size(); j++) {
+				map[i][j] = list.get(i).distance(list.get(j));
+				map[j][i] = map[i][j];
+			}
+		}
+		System.out.println(map);
 		// new Map(list.size)
 		// for(i = 0 to list.size - 1)
 		// for(j = i + 1 to list.size - 1)
 		// map[i,j] = distance(list[i], list[j])
 		// map[j,i] = map[i,j]
+	}
+
+	public static void main(String[] args) {
+		Map map = new Map();
+//		for(File file: folder.listFiles()) {
+//			String name = file.getName().substring(0, file.getName().length() - 4);
+//			map.readTest(file);
+//		}
+		try {
+			map.readTest(new File("dataset/a280.tsp"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
